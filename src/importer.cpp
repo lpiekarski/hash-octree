ef48edd139f7bcb9788cfc7e338a628d5fbb0541
lpiekarski <wookashp98@gmail.com>
2019-07-13 13:53:53 +0200

    created importer

--- src/importer.cpp
+++ src/importer.cpp
@@ -1,5 +1,38 @@
-//
-// Created by wooka on 7/13/2019.
-//
-
 #include "importer.h"
+
+namespace HashOctree {
+
+    HashOctree Importer::fromByteArray(const std::vector<char> &array) {
+        HashOctree ret;
+        size_t read_id = 0;
+        ret.root = read_uint64_t(array, read_id);
+        read_id += 8;
+
+        while (read_id < array.size()) {
+            uint64_t key = read_uint64_t(array, read_id);
+            read_id += 8;
+            uint64_t data = read_uint64_t(array, read_id);
+            read_id += 8;
+            uint64_t children[8];
+            for (int i = 0; i < 8; i++) {
+                children[i] = read_uint64_t(array, read_id);
+                read_id += 8;
+            }
+            ret.nodes[key].key = key;
+            ret.nodes[key].node.data = (void *) data;
+            for (int i = 0; i < 8; i++)
+                ret.nodes[key].node.children[i] = children[i];
+        }
+
+        ret.recountRefs();
+
+        return ret;
+    }
+
+    HashOctree Importer::fromJson(const std::string &str) {
+        HashOctree ret;
+
+        return ret;
+    }
+
+}
--- src/importer.h
+++ src/importer.h
@@ -1,8 +1,20 @@
-//
-// Created by wooka on 7/13/2019.
-//
-
 #ifndef HASH_OCTREE_IMPORTER_H
 #define HASH_OCTREE_IMPORTER_H
+
+#include "hash-octree.h"
+#include "point-data.h"
+
+namespace HashOctree {
+
+    class Importer {
+    public:
+        static HashOctree fromByteArray(const std::vector<char> &array);
+
+        static HashOctree fromJson(const std::string &str);
+
+        static HashOctree fromPointData(const PointData &pdt);
+    };
+
+}
 
 #endif //HASH_OCTREE_IMPORTER_H
