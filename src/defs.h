496a160b2881dcc9bd09c64d92b6ca2d04ff650b
lpiekarski <wookashp98@gmail.com>
2019-07-13 14:20:51 +0200

    created defs

--- src/defs.h
+++ src/defs.h
@@ -1,8 +1,26 @@
-//
-// Created by wooka on 7/13/2019.
-//
-
 #ifndef HASH_OCTREE_DEFS_H
 #define HASH_OCTREE_DEFS_H
+
+#include <cstdint>
+
+namespace HashOctree {
+    using key_t = uint64_t;
+    using dim_t = double;
+    using status_t = int64_t;
+
+    const status_t INDEX_OUT_OF_BOUNDS = -7;
+    const status_t KEY_COLLISION       = -6;
+    const status_t CHILD_DOESNT_EXIST  = -5;
+    const status_t NODE_DOESNT_EXIST   = -4;
+    const status_t NODE_IS_ROOT        = -3;
+    const status_t NODE_HAS_REFS       = -2;
+    const status_t NODE_EXISTS         = -1;
+    const status_t OK                  = 0;
+    const status_t NODE_REPLACED       = 1;
+
+    const int      FL_REC              = 1;
+    const int      FL_IGNORE_REFS      = 2;
+    const int      FL_FORCE            = 4;
+}
 
 #endif //HASH_OCTREE_DEFS_H
