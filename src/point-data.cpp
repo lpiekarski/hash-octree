275079c9fd5f74407afe6792b641f186e351d8d0
lpiekarski <wookashp98@gmail.com>
2019-07-13 13:51:53 +0200

    created point-data

--- src/point-data.cpp
+++ src/point-data.cpp
@@ -1,4 +1,23 @@
-//
-// Created by wooka on 7/13/2019.
-//
+#include "point-data.h"
+#include "data-utils.h"
 
+namespace HashOctree {
+
+    std::ostream& operator<<(std::ostream &os, const Point &p) {
+        if (os.binary) {
+            std::vector<char> v;
+            //requires implementation of push_float in data-utils
+            //push_int(v, ())
+            //os.write()
+        } else {
+            os << p.origin[0] << " " << p.origin[1] << " " << p.origin[2] << " ";
+            os << p.halfDim[0] << " " << p.halfDim[1] << " " << p.halfDim[2] << " ";
+            os << (uint64_t) p.data;
+        }
+
+        return os;
+    }
+
+    
+
+}
--- src/point-data.h
+++ src/point-data.h
@@ -1,8 +1,31 @@
-//
-// Created by wooka on 7/13/2019.
-//
-
 #ifndef HASH_OCTREE_POINT_DATA_H
 #define HASH_OCTREE_POINT_DATA_H
+
+#include <fstream>
+#include <iostream>
+
+#include "hash-octree.h"
+
+namespace HashOctree {
+
+    class Point {
+    public:
+        dim_t origin[3];
+        dim_t halfDim[3];
+        void *data;
+
+        friend std::istream& operator>>(std::istream &is, const Point &p);
+
+        friend std::ostream& operator<<(std::ostream &os, const Point &p);
+    };
+
+    class PointData {
+    public:
+        PointData(std::string filename);
+
+        int readAll(std::string filename);
+    };
+
+}
 
 #endif //HASH_OCTREE_POINT_DATA_H
