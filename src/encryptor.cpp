70190b0d87500ee430a3d0a8bb90900852abcd8c
lpiekarski <wookashp98@gmail.com>
2019-07-13 14:20:34 +0200

    created encryptor

--- src/encryptor.cpp
+++ src/encryptor.cpp
@@ -1,5 +1,33 @@
-//
-// Created by wooka on 7/13/2019.
-//
+#include <cstdlib>
 
 #include "encryptor.h"
+
+namespace HashOctree {
+
+    const uint64_t PRIME_P = 32416187567;
+    const uint64_t PRIME_Q = 1610612741;
+
+    key_t Encryptor::encrypt(const key_t *children, const void *data) {
+        key_t ret, p;
+
+        p = PRIME_P;
+        ret = (key_t) data * PRIME_Q;
+        for (size_t i = 0; i < 8; i++) {
+            ret += (children[i] * p);
+            p *= PRIME_P;
+        }
+
+        return ret;
+    }
+
+    key_t Encryptor::encrypt(const void *data) {
+        key_t empty_children[8];
+        for (size_t i = 0; i < 8; i++)
+            empty_children[i] = 0;
+        return Encryptor::encrypt(empty_children, data);
+    }
+
+    key_t Encryptor::encrypt(const Node &node) {
+        return Encryptor::encrypt(node.children, node.data);
+    }
+}
--- src/encryptor.h
+++ src/encryptor.h
@@ -1,8 +1,54 @@
-//
-// Created by wooka on 7/13/2019.
-//
-
 #ifndef HASH_OCTREE_ENCRYPTOR_H
 #define HASH_OCTREE_ENCRYPTOR_H
+
+#include "defs.h"
+#include "node.h"
+
+namespace HashOctree {
+
+    class Encryptor {
+    public:
+        /**
+         * Produces a key for a given node.
+         * Nodes with the same children keys
+         * and same data field value always
+         * have the same key. Also a key of
+         * a node with all children keys
+         * equal to 0 and nullptr data value
+         * is also 0.
+         * @param node HashOctree node object to be encrypted
+         * @return key produced from the node object
+         */
+        static key_t encrypt(const Node &node);
+
+        /**
+         * Produces a key for a given node.
+         * Nodes with the same children keys
+         * and same data field value always
+         * have the same key. Also a key of
+         * a node with all children keys
+         * equal to 0 and nullptr data value
+         * is also 0.
+         * @param children children key values of the node object
+         * @param data data field value of the node object
+         * @return key produced from the node object
+         */
+        static key_t encrypt(const key_t *children, const void *data);
+
+        /**
+         * Produces a key for a given node.
+         * Nodes with the same children keys
+         * and same data field value always
+         * have the same key. Also a key of
+         * a node with all children keys
+         * equal to 0 and nullptr data value
+         * is also 0.
+         * @param data data field value of the node object
+         * @return key produced from the node object with
+         * @p data and children keys equal to 0.
+         */
+        static key_t encrypt(const void *data);
+    };
+}
 
 #endif //HASH_OCTREE_ENCRYPTOR_H
