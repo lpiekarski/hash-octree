7c6ec598da85b279005e0ba4ae6f88aec9f95245
lpiekarski <wookashp98@gmail.com>
2019-07-13 14:21:10 +0200

    created data-utils

--- src/data-utils.cpp
+++ src/data-utils.cpp
@@ -1,5 +1,26 @@
-//
-// Created by wooka on 7/13/2019.
-//
-
 #include "data-utils.h"
+
+template <typename T, typename>
+int read_int(const std::vector<char> &v, size_t pos, T &ret) {
+    size_t bytes = sizeof(T);
+    ret = 0;
+
+    while (bytes--) {
+        ret = ret << 8;
+        ret += (unsigned char)v[pos + bytes];
+    }
+
+    return sizeof(T);
+}
+
+template <typename T, typename>
+int push_int(std::vector<char> &v, T val)  {
+    size_t bytes = sizeof(T);
+
+    while (bytes--) {
+        v.push_back(val & 255);
+        val = val >> 8;
+    }
+
+    return sizeof(T);
+}
--- src/data-utils.h
+++ src/data-utils.h
@@ -1,8 +1,17 @@
-//
-// Created by wooka on 7/13/2019.
-//
-
 #ifndef HASH_OCTREE_DATA_UTILS_H
 #define HASH_OCTREE_DATA_UTILS_H
+
+#include <cstdint>
+#include <cstdlib>
+#include <vector>
+#include <type_traits>
+
+template <typename T,
+        typename = std::enable_if_t<std::is_integral_v<T>>>
+int read_int(const std::vector<char> &v, size_t pos, T &ret);
+
+template <typename T,
+        typename = std::enable_if_t<std::is_integral_v<T>>>
+int push_int(std::vector<char> &v, T val);
 
 #endif //HASH_OCTREE_DATA_UTILS_H
