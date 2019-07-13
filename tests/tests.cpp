49f73745d1d8282351c28b9aa0f5ca74c293960b
lpiekarski <wookashp98@gmail.com>
2019-07-13 13:50:47 +0200

    created tests directory

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,10 +1,40 @@
-#include <iostream>
+#include "tests.h"
+#include <fstream>
+#include "../src/hash-octree.h"
 
-#include "hash-octree.h"
+#define TESTS_NUM 3
+
+int test_01() {
+    testInfoLog(stdout, "Exporter test");
+    HashOctree::HashOctree ho;
+    ho.addDataPoint(3, 3, 3, 2, 1, 1, (void*)123123);
+    std::ofstream p;
+    p.open("octree.json");
+    p << HashOctree::Exporter::toJson(ho, 4, true);
+    p.close();
+    return TEST_SUCCESS;
+}
+
+int test_02() {
+    testInfoLog(stdout, "Importer test")
+    return TEST_SUCCESS;
+}
+
+int test_03() {
+    testInfoLog(stdout, "HashOctree::addDataPoint test");
+    HashOctree::HashOctree ho;
+    assertNoException(ho.addDataPoint(10, 10, 10, 1, 1, 1, (void*)5));
+    return TEST_SUCCESS;
+}
+
+int (*tests[TESTS_NUM])(void) = {
+        test_01,
+        test_02,
+        test_03,
+};
 
 int main() {
-    HashOctree::HashOctree ho;
-    ho.addDataPoint(8, 8, 8, 8, 8, 8, (void *)5);
-    std::cout << HashOctree::Exporter::toString(ho);
+    for (auto test_func : tests)
+        testStatusLog(stdout, test_func);
     return 0;
 }
--- tests/tests.h
+++ tests/tests.h
@@ -1,8 +1,70 @@
-//
-// Created by wooka on 7/12/2019.
-//
+#if !defined(__TESTS_H__)
+#define __TESTS_H__
 
-#ifndef HASH_OCTREE_TESTS_H
-#define HASH_OCTREE_TESTS_H
+#include <chrono>
+#include <cstdio>
+#include <exception>
+#include <future>
+#include <string>
+#include <thread>
 
-#endif //HASH_OCTREE_TESTS_H
+#define TEST_ASSERTION_FAILED (0)
+#define TEST_SUCCESS (1)
+#define TEST_EXCEPTION (2)
+#define TEST_TIMEOUT (3)
+
+#define TIMEOUT_SECONDS (20)
+
+std::string testContext;
+
+const char *test_result[] = {
+        "assertion failed",
+        "success",
+        "exception",
+        "timeout"
+};
+
+#define assert(pred)\
+    if (!(pred)) {\
+        fprintf(stderr,\
+                "%s:%d assertion failed\n",\
+                __func__,\
+                __LINE__\
+                );\
+        return TEST_ASSERTION_FAILED;\
+    }
+
+#define assertNoException(prog)\
+    try {\
+        prog;\
+    } catch (const std::exception &e) {\
+        fprintf(stderr,\
+                "%s:%d noexcept assertion failed: %s\n",\
+                __func__,\
+                __LINE__,\
+                e.what()\
+                );\
+        return TEST_EXCEPTION;\
+    }
+
+#define testInfoLog(stream, msg)\
+    if (testContext == __func__) {\
+        fprintf((stream), "\t%s\n",  (msg));\
+        fflush((stream));\
+    } else {\
+        fprintf((stream), "%s: %s\n",  __func__, (msg));\
+        fflush((stream));\
+        testContext = __func__;\
+    }
+
+#define testStatusLog(stream, func) {\
+        auto __future = std::async(std::launch::async, (func));\
+        size_t __result;\
+        if (__future.wait_for(std::chrono::seconds(TIMEOUT_SECONDS)) != std::future_status::ready)\
+            __result = TEST_TIMEOUT;\
+        else\
+            __result = __future.get();\
+        fprintf((stream), "\t%s test result: %s\n\n",  testContext.c_str(), test_result[__result]);\
+    }
+
+#endif // __TESTS_H__
