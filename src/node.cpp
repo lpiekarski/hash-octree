b2e2ae19d7a3d0775b57e50269ece3ef840fff83
lpiekarski <wookashp98@gmail.com>
2019-07-13 13:53:25 +0200

    created node

--- src/node.cpp
+++ src/node.cpp
@@ -1,5 +1,13 @@
-//
-// Created by wooka on 7/13/2019.
-//
+#include <cstdlib>
 
 #include "node.h"
+
+namespace HashOctree {
+
+    Node::Node(const key_t *children, const void *data) {
+        this->data = (void *)data;
+        for (size_t i = 0; i < 8; i++)
+            this->children[i] = children[i];
+    }
+
+}
--- src/node.h
+++ src/node.h
@@ -1,8 +1,35 @@
-//
-// Created by wooka on 7/13/2019.
-//
-
 #ifndef HASH_OCTREE_NODE_H
 #define HASH_OCTREE_NODE_H
+
+#include "defs.h"
+
+namespace HashOctree {
+
+    class Node {
+    public:
+        key_t children[8];
+        void *data;
+
+        Node() = default;
+
+        Node(const key_t *children, const void *data);
+    };
+
+    class NodeControlBlock {
+    public:
+        key_t key;
+        Node node;
+        int32_t refs;
+    };
+
+    class NodeOperationBlock {
+    public:
+        NodeControlBlock *ncb;
+        dim_t origin[3];
+        dim_t halfDim[3];
+        NodeControlBlock *parent;
+    };
+
+}
 
 #endif //HASH_OCTREE_NODE_H
