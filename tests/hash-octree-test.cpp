521777768eec1386cf1fff4cbabfc12a7e16458a
lpiekarski <wookashp98@gmail.com>
2019-07-21 13:47:30 +0200

    added empty template parameter list

--- tests/hash-octree-test.cpp
+++ tests/hash-octree-test.cpp
@@ -8,7 +8,7 @@
 
 int test_01() {
     testInfoLog(stdout, "Exporter test");
-    HashOctree::HashOctree ho;
+    HashOctree::HashOctree<> ho;
     ho.addDataPoint(HashOctree::NodeDims(10, 3, 23, 4, 5, 7), (void*)123123);
     ho.addDataPoint(HashOctree::NodeDims(-3, 5, 7, 1, 2, 3), (void*)1233);
     ho.addDataPoint(HashOctree::NodeDims(2, 5, 3, 10, 3, 3), (void*)555123);
@@ -26,7 +26,7 @@ int test_02() {
 
 int test_03() {
     testInfoLog(stdout, "HashOctree::addDataPoint test");
-    HashOctree::HashOctree ho;
+    HashOctree::HashOctree<> ho;
     assertNoException(ho.addDataPoint(HashOctree::NodeDims(10, 10, 10, 1, 1, 1), (void*)5));
     return TEST_SUCCESS;
 }
